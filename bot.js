3dc6b65c6a4f8c85e7aa6800a3bd92e5ab24bde2
gobbija <49251345+gobbija@users.noreply.github.com>
2019-04-04 21:05:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,15 @@ let cdseconds = 5;
 
 const http = require('http');
 
+process
+  .on('SIGTERM', shutdown('SIGTERM'))
+  .on('SIGINT', shutdown('SIGINT'))
+  .on('uncaughtException', shutdown('uncaughtException'));
+
+setInterval(console.log.bind(console, 'tick'), 1000);
+http.createServer((req, res) => res.end('hi'))
+  .listen(process.env.PORT || 3000, () => console.log('Listening'));
+
 
 function shutdown(signal) {
   return (err) => {
