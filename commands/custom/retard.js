0a3f8c9924155c0b2ec634bd3d0547e18e0c1405
gobbija <49251345+gobbija@users.noreply.github.com>
2019-04-04 16:22:27 +0300

    Add files via upload

---
+++ commands/custom/retard.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+
+var VC = message.member.voiceChannel;
+
+if (!VC)
+
+return message.reply("Tev vajag būt voice kanala, anton")
+
+VC.join()
+
+.then(connection => {
+
+const dispatcher = connection.playFile('./audio/suka.mp3');
+
+ dispatcher.on("end", end => {VC.leave()});
+
+})
+
+.catch(console.error);
+}
+
+module.exports.help = {
+  name: "retard"
+}
